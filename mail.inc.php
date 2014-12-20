3fa53743b2f61bcee32633ae77b4959b05b2909b
Bojie Li <bojieli@gmail.com>
2014-12-20 17:02:36 +0800

    change email from

--- mail.inc.php
+++ mail.inc.php
@@ -1,7 +1,7 @@
 <?php
 function utf8_mail($to, $title, $content) {
     $mail_headers  = "Content-type: text/plain; charset=utf-8\r\n";
-    $mail_headers .= "From: Service Monitor <nobody@blog.ustc.edu.cn>\r\n";
+    $mail_headers .= "From: Service Monitor <nobody@servmon.lug.ustc.edu.cn>\r\n";
 
     $title = '=?utf-8?B?'.base64_encode($title).'?=';
     mail($to, $title, $content, $mail_headers);
